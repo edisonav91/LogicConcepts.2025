22e4d2bdc7fe1cbc6cb03d09fa92e0d6c82b4835
Edison Alvarez <edisonav1991@gmail.com>
2025-02-16 15:31:06 -0500

    Complete exercice 01

---
+++ LogicConcepts/isNumberOddOrNot/Program.cs
@@ -0,0 +1,24 @@
+﻿do
+{
+    Console.Write("Ingrese numero o presiones ctrl c para salir: ");
+    var numberString = Console.ReadLine();
+    var numberInt = 0;
+    if(int.TryParse(numberString, out numberInt))
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El numero {numberInt} es par: ");
+
+        }
+        else
+        {
+            Console.WriteLine($"El numero {numberInt} es impar: ");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Lo que ingresaste: {numberString} no es un numero entero.");
+    }
+} while (true);
+
+
